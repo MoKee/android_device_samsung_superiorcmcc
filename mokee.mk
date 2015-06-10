5a3bf0f031fb935b6fe140545b3ee30b93b1d4e0
ljzyal <ljzyal@gmail.com>
2015-06-11 00:02:16 +0800

    superiorcmcc:mokee bringup
    Change-Id: I9b16507756d7bf8f29e914ccb9c853810ee012d7

--- mokee.mk
+++ mokee.mk
@@ -21,18 +21,18 @@ PRODUCT_RELEASE_NAME := GT-I9268
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Mokee stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/superiorcmcc/full_superiorcmcc.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := superiorcmcc
-PRODUCT_NAME := cm_superiorcmcc
+PRODUCT_NAME := mk_superiorcmcc
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9268
 PRODUCT_MANUFACTURER := samsung
